474d0283315204edec9d3d94c885c6f039bbb756
Laura Brea <laurabrea17@gmail.com>
2023-03-04 13:40:26 -0500

    testing out if the deployment will work now

--- index.js
+++ index.js
@@ -38,3 +38,5 @@ app.listen(80, function () {
     console.log('CORS-enabled web server listening on port 80')
 })
 app.listen(PORT, console.log(`listening on port ${PORT}`))
+
+module.exports = app
